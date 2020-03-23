ae9dcb52df3497b4d3899833ade46e3a266def7b
PEREZ CASTRO PHILIP JUNIOR <jperez.c92@gmail.com>
2020-03-23 18:01:16 -0500

    subido a firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: 'AIzaSyA85giftI8Y3HM6CQxUs8JMC6hSmPJkLSs',
+    authDomain: 'ngblog-dae47.firebaseapp.com',
+    databaseURL: 'https://ngblog-dae47.firebaseio.com',
+    projectId: 'ngblog-dae47',
+    storageBucket: 'ngblog-dae47.appspot.com',
+    messagingSenderId: '266964956073',
+    appId: '1:266964956073:web:a9dabc40d34d1ea1cf4ab8',
+    measurementId: 'G-SM58FE44F1'
+  }
 };
